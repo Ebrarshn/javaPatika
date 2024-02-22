4d192993268b0170b22d202bb1c2a5f6664970c9
Ebrarshn <159464276+Ebrarshn@users.noreply.github.com>
2024-02-22 23:45:06 +0300

    Hesap makinası(if-else)

--- src/javaPatika/hesapmakinesi.java
+++ src/javaPatika/hesapmakinesi.java
@@ -1,10 +1,37 @@
 package javaPatika;
 
+import java.util.Scanner;
+
 public class hesapmakinesi {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		int n1, n2, select;
+		
+		Scanner inp=new Scanner(System.in);
+		System.out.print("İlk sayıyı giriniz: ");
+		n1=inp.nextInt();
+		System.out.print("İkinci sayıyı giriniz: ");
+		n2=inp.nextInt();
+		
+		System.out.println("1-toplam\n2-çıkartma\n3-çarpma\n4-bölme");
+		System.out.print("seçiminiz: ");
+		select=inp.nextInt();
+		
+		if(select == 1) {
+			System.out.print("toplam: "+ (n1+n2));
+		}else if(select == 2) {
+			System.out.print("çıkartma: "+ (n1-n2));
+		}else if(select == 3) {
+			System.out.print("çarpma: "+ (n1*n2));
+		}else if(select == 4) {
+			if (n2 != 0) {
+				System.out.print("bölme: "+ (n1/n2));
+			}else {
+				System.out.print("bir sayı 0 ile bölünmez !");
+			}
+		}else {
+			System.out.print("yanlış seçim yaptınız.Tekrar deneyiniz.");
+		}
+		}
 
 }
